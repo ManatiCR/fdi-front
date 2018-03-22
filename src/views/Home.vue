308bf7988c2355026c3d6599c08bc93d6ecf824b
Andrés Díaz Soto <andres@estudiomanati.com>
2018-03-21 18:25:33 -0600

    FDI-16: Add content blocks to homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="view-home">
-    <h1>Home</h1>
+    <content-block id="home_reporte"></content-block>
+    <content-block id="home_derechos"></content-block>
+    <content-block id="home_fdi"></content-block>
+    <content-block id="home_espacios"></content-block>
   </div>
 </template>
+
+<script>
+import ContentBlock from '../components/ContentBlock.vue';
+export default {
+  components: {
+    ContentBlock,
+  }
+};
+</script>
