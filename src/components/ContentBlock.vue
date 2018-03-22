d25365fefa904526e5c5899346535d2771e33da2
Andrés Díaz Soto <andres@estudiomanati.com>
2018-03-21 18:25:01 -0600

    FDI-16: Add Content Block component

---
+++ src/components/ContentBlock.vue
@@ -0,0 +1,78 @@
+<template>
+  <section>
+    <h1>{{ nodeQuery.title }}</h1>
+    <div v-html="nodeQuery.body"></div>
+  </section>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+
+const query = gql`query($id: String!) {
+  nodeQuery(limit: 10,
+    offset: 0,
+    filter: {
+      conditions: [
+        {
+          operator: EQUAL,
+          field: "type",
+          value: ["bloque_de_contenido"]
+        },
+        {
+          operator: EQUAL,
+          field: "field_tipo",
+          value: [$id]
+        }
+      ]
+    }) {
+    entities {
+      entityLabel,
+      ... on NodeBloqueDeContenido {
+        body {
+          value
+        },
+        fieldImagen {
+          url
+        }
+      }
+    }
+  }
+}`;
+
+export default {
+  name: 'ContentBlock',
+  props: [
+    'id'
+  ],
+  apollo: {
+    nodeQuery() {
+      return {
+        query: query,
+        variables: {
+          id: this.id
+        },
+        update(data) {
+          console.log('data', data);
+          if (data.hasOwnProperty('nodeQuery') &&
+            data.nodeQuery.hasOwnProperty('entities') &&
+            data.nodeQuery.entities instanceof Array &&
+            data.nodeQuery.entities.length === 1) {
+            const entity = data.nodeQuery.entities[0];
+            return {
+              title: entity.entityLabel,
+              body: entity.body.value,
+              image: entity.fieldImagen,
+            }
+          }
+        }
+      }
+    }
+  },
+  data() {
+    return {
+      title: '',
+      nodeQuery: {},
+    }
+  },
+}
+</script>
