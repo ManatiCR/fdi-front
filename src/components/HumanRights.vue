3a309d49352ec05caa7e29d028d11213bd780388
Josue2591 <josuegv2591@gmail.com>
2018-04-03 11:51:31 -0600

    FDI-21: Create HumanRights component.

---
+++ src/components/HumanRights.vue
@@ -0,0 +1,104 @@
+<template>
+  <section class="human-rights">
+    <ul class="human-rights__list">
+      <li class="human-rights__item" v-for="(entity, index) in nodeQuery.entities">
+        <img class="human-rights__img" :src="nodeQuery.entities[index].fieldImagen.derivative.url" alt="">
+        <h3 class="human-rights__title">{{nodeQuery.entities[index].entityLabel}}</h3>
+        <div class="human-rights__body" v-html="nodeQuery.entities[index].body.value"></div>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+
+const query = gql `query {
+  nodeQuery(limit: 6,
+    sort: [
+      {
+        field: "created",
+        direction: DESC
+      }
+    ],
+    filter: {
+      conditions: [
+        {
+          operator: EQUAL,
+          field: "type",
+          value: ["derecho"]
+        }
+      ]
+    }) {
+      entities {
+        entityLabel
+        ... on NodeDerecho {
+          fieldImagen {
+            derivative (style: medium) {
+              url
+            }
+          }
+          body {
+            value
+          }
+        }
+      }
+    }
+  }`;
+
+export default {
+  name: 'HumanRights',
+  apollo: {
+    nodeQuery() {
+      return {
+        query: query,
+      }
+    }
+  },
+  data() {
+    return {
+      nodeQuery: {},
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+@import "../assets/scss/variables";
+
+.human-rights__list {
+  max-width: 1440px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 100%;
+  padding: 0 20px;
+  margin: 0 auto;
+}
+
+.human-rights__item {
+  width: 100%;
+  max-width: 400px;
+  padding: 5px;
+  list-style: none;
+  margin-bottom: 20px;
+  display: block;
+  text-align: center;
+  @media (min-width: 480px) {
+    width: 45%;
+  }
+  @media (min-width: $bp-medium) {
+    width: 32%;
+    margin-bottom: 0;
+  }
+}
+
+.human-rights__img {
+  max-width: 100%;
+  height: auto;
+}
+
+.human-rights__body {
+  text-align: left;
+}
+</style>
