d572b7f136eb5455fdd304547bb9ecc01df65d7c
Josue2591 <josuegv2591@gmail.com>
2018-04-24 05:54:43 -0600

    Add conditional render fields for humanrights component.

--- src/components/HumanRights.vue
+++ src/components/HumanRights.vue
@@ -2,9 +2,9 @@
   <section class="human-rights">
     <ul class="human-rights__list">
       <li class="human-rights__item" v-for="(entity, index) in nodeQuery.entities">
-        <img :width="(entity.fieldImagen.derivative.width/2)" :height="(entity.fieldImagen.derivative.height/2)" class="human-rights__img" :src="entity.fieldImagen.derivative.url" :alt="entity.entityLabel">
+        <img v-if="entity.fieldImagen" :width="(entity.fieldImagen.derivative.width/2)" :height="(entity.fieldImagen.derivative.height/2)" class="human-rights__img" :src="entity.fieldImagen.derivative.url" :alt="entity.entityLabel">
         <h3 class="human-rights__title">{{entity.entityLabel}}</h3>
-        <div class="human-rights__body" v-html="entity.body.value"></div>
+        <div v-if="entity.body"class="human-rights__body" v-html="entity.body.value"></div>
       </li>
     </ul>
   </section>
