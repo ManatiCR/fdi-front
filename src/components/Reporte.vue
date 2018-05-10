680da1764c628e52de850345ff05f9c7a70525fb
Josue2591 <josuegv2591@gmail.com>
2018-05-10 16:34:16 -0600

    FDI-50: Add initial styles for reporte form.

--- src/components/Reporte.vue
+++ src/components/Reporte.vue
@@ -1,94 +1,135 @@
 <template>
   <div class="create-report">
-    <header class="create-report__header">
-    <h1>Reportá</h1>
-    <h2 v-if="formStep">{{ stepTitle[formStep] }}</h2>
-    </header>
-    <div v-if="formStep === 0" class="create-report__step-0">
-      <content-block id="reporte"></content-block>
-      <button @click="gotoStep(1)" class="btn btn--xlarge btn--border-highlight1">Reportá</button>
+    <div class="create-report__wrapper">
+      <h1 v-if="formStep" class="create-report__title">Reportá</h1>
+      <div class="create-report__steps-wrapper">
+        <header class="create-report__header">
+        <h2 v-if="formStep">{{ stepTitle[formStep] }}</h2>
+        </header>
+        <div v-if="formStep === 0" class="create-report__step-0">
+          <content-block class="reporte" id="reporte"></content-block>
+          <button
+            @click="gotoStep(1)"
+            class="btn create-report__btn btn--xlarge btn--border-highlight1">
+            Reportá
+          </button>
+        </div>
+        <div v-if="formStep === 1" class="create-report__step create-report__step-1">
+          <multiselect
+          placeholder="Seleccione una categoría"
+          select-label="Agregar"
+          deselectLabel="Eliminar"
+          selectedLabel="Seleccionada"
+          v-model="categories"
+          :options="availableCategories.categories.entities"
+          track-by="entityId"
+          label="entityLabel"
+          :multiple="true"
+          :searchable="true"
+          ></multiselect>
+          <multiselect
+          placeholder="Seleccione una subcategoría"
+          select-label="Agregar"
+          deselectLabel="Eliminar"
+          selectedLabel="Seleccionada"
+          v-if="categories.length"
+          v-model="subcategories"
+          :options="availableCategories.subcategories.entities"
+          track-by="entityId"
+          label="entityLabel"
+          :multiple="true"
+          :searchable="true"
+          ></multiselect>
+          <button
+            @click="gotoStep(2)"
+            class="btn btn--fill-highlight1 btn--arrow">
+            Continuar
+          </button>
+        </div>
+        <div v-if="formStep === 2" class="create-report__step create-report__step-2">
+          <div class="create-report__step-2-content">
+            <gmap-map
+            ref="map"
+            :center="center"
+            :zoom="zoom"
+            style="width: 500px; height: 300px"
+            >
+              <gmap-marker
+              v-if="locationStatus !== 'unset'"
+              :position="markerPosition"
+              :draggable="markerDraggable"
+              @dragend="markerDragged"
+              ></gmap-marker>
+            </gmap-map>
+            <ul>
+              <li @click="goToCurrentLocation()">Ubicación Actual</li>
+              <li
+                v-for="(place, index) in places"
+                :key="index"
+                @click="goToPlace(place)"
+              >
+                {{ place.name }}
+              </li>
+            </ul>
+          </div>
+          <button
+            @click="gotoStep(3)"
+            class="btn btn--fill-highlight1
+            btn--arrow">
+            Continuar
+          </button>
+        </div>
+        <div v-if="formStep === 3" class="create-report__step create-report__step-3">
+          <textarea v-model="reportText"></textarea>
+          <button
+            @click="gotoStep(4)"
+            class="btn btn--fill-highlight1 btn--arrow">
+            Continuar
+          </button>
+        </div>
+        <div v-if="formStep === 4" class="create-report__step create-report__step-4">
+          <p>
+            El FDI te puede brindar el apoyo y asesoría necesaria para realizar una denuncia formal.
+            Este acompañamiento sería durante todo el proceso, en caso de que decidás continuar.
+          </p>
+          <div class="create-report__step-4-btn-wrapper">
+            <button
+              @click="gotoStep(5)"
+              class="btn btn--fill-highlight1">
+              Sí quiero
+            </button>
+            <button
+              @click="gotoStep(6)"
+              class="btn create-report__btn-end btn--fill-background3">
+              No, finalizar
+            </button>
+          </div>
+        </div>
+        <div v-if="formStep === 5" class="create-report__step create-report__step-5">
+          <p>
+            El FDI te puede brindar el apoyo y asesoría necesaria para realizar una denuncia formal.
+            Este acompañamiento sería durante todo el proceso, en caso de que decidás continuar.
+          </p>
+          <input type="text" placeholder="Nombre" v-model="personName">
+          <input type="email" placeholder="Correo electrónico" v-model="personEmail">
+          <input type="text" placeholder="Teléfono" v-model="personPhone">
+          <button @click="gotoStep(6)" class="btn btn--fill-highlight1">Enviar</button>
+        </div>
+        <div v-if="formStep === 6" class="create-report__step create-report__step-6">
+          <p>Guarda el siguiente código, en caso de que cambies de opinión:</p>
+          <router-link
+            :to="{ name: 'home'}"
+            class="btn btn--fill-highlight1">
+            Finalizar
+          </router-link>
+        </div>
+      </div>
     </div>
-    <div v-if="formStep === 1" class="create-report__step-1">
-      <h2>Step 1</h2>
-      <multiselect
-      placeholder="Seleccione una categoría"
-      v-model="categories"
-      :options="availableCategories.categories.entities"
-      track-by="entityId"
-      label="entityLabel"
-      :multiple="true"
-      :searchable="true"
-      ></multiselect>
-      <multiselect
-      placeholder="Seleccione una subcategoría"
-      v-if="categories.length"
-      v-model="subcategories"
-      :options="availableCategories.subcategories.entities"
-      track-by="entityId"
-      label="entityLabel"
-      :multiple="true"
-      :searchable="true"
-      ></multiselect>
-      <button @click="gotoStep(2)" class="btn btn--fill-highlight1 btn--arrow">Continuar</button>
-    </div>
-    <div v-if="formStep === 2" class="create-report__step-2">
-      <gmap-map
-      ref="map"
-      :center="center"
-      :zoom="zoom"
-      style="width: 500px; height: 300px"
-      >
-        <gmap-marker
-        v-if="locationStatus !== 'unset'"
-        :position="markerPosition"
-        :draggable="markerDraggable"
-        @dragend="markerDragged"
-        ></gmap-marker>
-      </gmap-map>
-      <ul>
-        <li @click="goToCurrentLocation()">Ubicación Actual</li>
-        <li
-          v-for="(place, index) in places"
-          :key="index"
-          @click="goToPlace(place)"
-        >
-          {{ place.name }}
-        </li>
-      </ul>
-      <button @click="gotoStep(3)" class="btn btn--fill-highlight1 btn--arrow">Continuar</button>
-    </div>
-    <div v-if="formStep === 3" class="create-report__step-3">
-      <textarea v-model="reportText"></textarea>
-      <button @click="gotoStep(4)" class="btn btn--fill-highlight1 btn--arrow">Continuar</button>
-
-    </div>
-    <div v-if="formStep === 4" class="create-report__step-4">
-      <p>
-        El FDI te puede brindar el apoyo y asesoría necesaria para realizar una denuncia formal.
-        Este acompañamiento sería durante todo el proceso, en caso de que decidás continuar.
-      </p>
-      <button @click="gotoStep(5)" class="btn btn--fill-highlight1">Sí quiero</button>
-      <button @click="gotoStep(6)" class="btn btn--fill-background3">No, finalizar</button>
-    </div>
-    <div v-if="formStep === 5" class="create-report__step-5">
-      <p>
-        El FDI te puede brindar el apoyo y asesoría necesaria para realizar una denuncia formal.
-        Este acompañamiento sería durante todo el proceso, en caso de que decidás continuar.
-      </p>
-      <input type="text" placeholder="Nombre" v-model="personName">
-      <input type="email" placeholder="Correo electrónico" v-model="personEmail">
-      <input type="text" placeholder="Teléfono" v-model="personPhone">
-      <button @click="gotoStep(6)" class="btn btn--fill-highlight1">Enviar</button>
-    </div>
-    <div v-if="formStep === 6" class="create-report__step-6">
-      <p>Guarda el siguiente código, en caso de que cambies de opinión:</p>
-      <router-link :to="{ name: 'home'}" class="btn btn--fill-highlight1">Finalizar</router-link>
-    </div>
-
   </div>
 </template>
 
 <script>
+import 'vue-multiselect/dist/vue-multiselect.min.css';
 import Vue from 'vue';
 import gql from 'graphql-tag';
 import * as VueGoogleMaps from 'vue2-google-maps';
@@ -290,3 +331,201 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+@import "../assets/scss/variables";
+
+// TODO: Move this code (reporte rule) to ContentBlock component.
+.reporte {
+  text-align: left;
+  margin-bottom: 40px;
+  h1 {
+    font-size: 2.4rem;
+    display: inline-block;
+    margin: 0 0 40px 0;
+    padding-right: 50px;
+    border-right: solid 5px $highlight2;
+    color: $highlight1;
+   }
+   p {
+     font-size: 1.8rem;
+   }
+}
+///////////////////
+
+.create-report {
+  padding: 80px 20px;
+  .btn {
+    display: block;
+    margin: 0 auto;
+    @media (min-width: 1000px) {
+      margin: 0;
+      display: inline-block;
+    }
+  }
+}
+
+.create-report__wrapper {
+  background: #eef5fb;
+  padding: 40px 20px;
+  max-width: 1100px;
+  margin: 0 auto;
+  @media (min-width: 1000px) {
+    padding: 60px;
+    display: flex;
+    align-items: flex-start;
+  }
+}
+
+.create-report__wrapper .create-report__btn {
+  padding: 20px 60px;
+  @media (min-width: 1000px) {
+    padding: 20px 80px;
+    margin-left: 40px;
+  }
+}
+
+.create-report__steps-wrapper {
+  flex: 0 1 800px;
+  h2 {
+    margin: 0 0 20px 0;
+  }
+  @media (min-width: 1000px) {
+    margin-left: 60px;
+  }
+}
+
+.create-report__title {
+  font-size: 2.4rem;
+  display: inline-block;
+  margin: 0 0 40px 0;
+  padding-right: 50px;
+  border-right: solid 5px $highlight2;
+  color: $highlight1;
+}
+
+// Multiselect
+.multiselect {
+  max-width: 320px;
+  width: 100%;
+  margin-bottom: 40px;
+  @media (min-width: 1000px) {
+    width: 300px;
+  }
+}
+
+.multiselect__tag,
+.multiselect__tag-icon {
+  background: $highlight3;
+  border-radius: 2px;
+}
+
+.multiselect__tag-icon:focus,
+.multiselect__tag-icon:hover {
+  background: darken($highlight3, 10%);
+}
+
+.multiselect__option--highlight {
+  background: $highlight3;
+  &:after {
+    background: $highlight3;
+  }
+}
+// Multiselect --end
+
+.create-report__step-0 {
+  text-align: center;
+  @media (min-width: 1000px) {
+    display: flex;
+    align-items: center;
+  }
+}
+
+.create-report__step-2-content {
+  margin-bottom: 20px;
+  .vue-map-container {
+    margin: 0 auto 20px auto;
+    max-width: 100%;
+    height: auto;
+    width: 800px;
+  }
+  ul {
+    margin: 0 auto;
+    max-width: 500px;
+    background: #fff;
+    padding: 20px;
+    list-style: none;
+    height: 300px;
+    overflow: hidden;
+    scroll-behavior: smooth;
+    overflow-y: scroll;
+    border: 1px solid #e0e0e0;
+    @media (min-width: 1000px) {
+      margin-left: 20px;
+    }
+  }
+  @media (min-width: 1000px) {
+    display: flex;
+  }
+}
+
+.create-report__step-3 {
+  textarea {
+    max-width: 100%;
+    width: 100%;
+    min-height: 200px;
+    margin-bottom: 20px;
+    color: $text;
+    font-family: $opensans;
+    line-height: 1.5;
+    font-size: 1.6rem;
+  }
+}
+
+.create-report__step-4,
+.create-report__step-5,
+.create-report__step-6 {
+  p {
+    margin: 0 0 20px 0;
+    line-height: 1.5;
+    font-size: 1.6rem;
+  }
+}
+
+.create-report__step-4-btn-wrapper {
+  .btn {
+    width: 151px;
+    margin-bottom: 20px;
+    @media (min-width: 500px) {
+      display: inline-block;
+    }
+  }
+  @media (min-width: 500px) {
+    .btn.create-report__btn-end {
+      margin-left: 20px
+    }
+  }
+}
+
+.create-report__step-5 {
+  input {
+    height: 33px;
+    border: solid 1px #e0e0e0;
+    padding-left: 10px;
+    font-size: 1.6rem;
+  }
+  input, .btn {
+    display: block;
+    max-width: 360px;
+    width: 100%;
+    margin: 0 auto 20px auto;
+    @media (min-width: 500px) {
+      margin: 0 0 20px 0;
+    }
+  }
+  .btn {
+    max-width: 180px;
+  }
+}
+
+</style>
