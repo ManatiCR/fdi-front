e3fb88bb4526e37dd413137ca8a3eb9d66d4e2b5
Andrés Díaz Soto <andres@estudiomanati.com>
2018-05-28 13:56:16 -0600

    FDI-63: Change app name / Add RRSS sharing image

--- src/config.js
+++ src/config.js
@@ -1,12 +1,17 @@
 const config = {
   development: {
+    siteName: 'FDI Vigilancia Ciudadana',
+    baseUrl: 'http://localhost:8080',
     apiUrl: 'http://test-fdi.pantheonsite.io/graphql',
     ga: '',
+    ogImage: '/img/social.png',
   },
   production: {
+    siteName: 'FDI Vigilancia Ciudadana',
+    baseUrl: 'http://app.fdi.cr',
     apiUrl: 'https://live-fdi.pantheonsite.io/graphql',
     ga: 'UA-119942269-1',
-
+    ogImage: '/img/social.png',
   },
 };
 
